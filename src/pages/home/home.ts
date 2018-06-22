907efc1833b59fea6dd4dad1a054ff1f41ef15fc
Tim Doppenberg <tim.doppenberg@carapaxit.nl>
2018-06-22 16:32:05 +0200

    adds some stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+
+import { MainPage } from '../main/main';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild(Slides) slides: Slides;
+
+  skipMsg = 'Skip'
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  skip() {
+    this.navCtrl.push(MainPage);
+  }
+
+  slideChanged() {
+    if(this.slides.isEnd) {
+      this.skipMsg = 'Alright, I got it!';
+    }
   }
 
 }
